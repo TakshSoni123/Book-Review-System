4dc01af5ec0b88f9d186741a6511ed86a2277a58
Taksh Soni <sonitaksh@gmail.com>
2022-07-22 16:12:54 +0530

    Registration features added

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [data , setData] = React.useState(null);
+  // const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("/test")
-    .then((res) => res.json())
-    .then((data) => setData(data.message));
-  }, []);
+  // React.useEffect(() => {
+  //   fetch("/test")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.message));
+  // }, []);
+
+  const [usernameReg, setUserNameReg] = useState('')
+  const [passwordReg, setPasswordReg] = useState('')
+  const [rePasswordReg, setRePasswordReg] = useState('')
+
+  const register = () => {
+    Axios.post('http://localhost:3001/register', { username: usernameReg, password: passwordReg, repassword: rePasswordReg }).then((response) => {
+      console.log(response);
+    });
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "loading..." : data}</p>
-      </header>
+      <div className='register'>
+        <h1>Sign-up</h1>
+
+        <label>Username</label>
+        <input type="text" onChange={(e) => {
+          setUserNameReg(e.target.value);
+        }}></input>
+
+        <label>Password</label>
+        <input type="password" onChange={(e) => {
+          setPasswordReg(e.target.value);
+        }}></input>
+
+        <label>Re-type Password</label>
+        <input type="password" onChange={(e) => {
+          setRePasswordReg(e.target.value);
+        }}></input>
+
+        <button onClick={register}> Register </button>
+      </div>
+      <div className='login'>
+        <h1>Login</h1>
+        <input type="text" placeholder="Username..."></input>
+        <input type="password" placeholder="Password..."></input>
+        <button>Login</button>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,52 @@
 
 const express = require("express");
+const cors = require('cors');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/test", (req, res) => {
-    res.json({ message : "Test API!!!!" });
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", (req, res) => {
+
+    //check if username already exists
+    const username = req.body.username;
+    const password = req.body.password;
+    const rePassword = req.body.repassword;
+
+    if (password == rePassword) {
+        //read file and then update and then write
+        fs.readFile("../Books-plus-plus/Resources/LoginDetails.json", (err, fileData) => {
+            if (err) {
+                console.log("Failed to read file : " + err);
+            }
+            try {
+                var data = JSON.parse(fileData);
+                //need to hash password before saving
+                //Create user json 
+                const userDetails = {
+                    "username": username,
+                    "password": password
+                }
+
+                data[username] = userDetails;
+                fs.writeFile("../Books-plus-plus/Resources/LoginDetails.json", JSON.stringify(data), err => {
+                    if(err){
+                        console.log(err);
+                    }
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        });
+
+    }
+    else {
+        console.log("Passwords do not match");
+    }
 });
 
 app.listen(PORT, () => {
