498ca1c7d0e285452fe8ed35b9579048d57a64ee
Taksh Soni <sonitaksh@gmail.com>
2022-07-27 17:49:06 +0530

    login signup fetch APIs added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Axios from 'axios';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   // const [data, setData] = React.useState(null);
